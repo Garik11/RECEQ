87ca5a4f831373a5a9e5ac58f47e1c4744b06fc2
Garik11 <garik.irk@mail.ru>
2023-12-03 14:36:49 +0300

    main

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include "src/rec_solver.h"
 
 int main(void){
-    const char* eq = "";
+    const char* eq = "100+100*200+(1000/7)+(1000/((700+300)/70))";
     RecType output = RecCalculate(eq);
     printf("calculated num = %lld\n", output);
 }
